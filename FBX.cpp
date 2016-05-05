d0f14268e8e7ee18019816fae44278208e3a1118
horinoh <development@horino.sakura.ne.jp>
2016-05-05 15:27:04 +0900

    FBXまわりの準備

---
+++ FBX.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "vs2015\\x64\\debug\\libfbxsdk.lib")
+//#pragma comment(lib, "vs2015\\x64\\debug\\libfbxsdk-md.lib")
+//#pragma comment(lib, "vs2015\\x64\\debug\\libfbxsdk-mt.lib")
+#else
+#pragma comment(lib, "vs2015\\x64\\release\\libfbxsdk.lib")
+//#pragma comment(lib, "vs2015\\x64\\release\\libfbxsdk-md.lib")
+//#pragma comment(lib, "vs2015\\x64\\release\\libfbxsdk-mt.lib")
+#endif
