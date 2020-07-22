5c09bbbf2323e2cf3f526b389a2b587f348abbfc
horinoh <development@horino.sakura.ne.jp>
2020-07-22 23:15:59 +0900

    バグ修正

--- ComputeVK/ComputeVK.h
+++ ComputeVK/ComputeVK.h
@@ -66,9 +66,9 @@ protected:
 		}, DescriptorSetLayouts[0]);
 	}
 	virtual void UpdateDescriptorSet() override {
-		assert(!_ImageViews.empty() && "");
+		assert(!ImageViews.empty() && "");
 		const DescriptorUpdateInfo DUI = {
-			{ VK_NULL_HANDLE, _ImageViews[0], VK_IMAGE_LAYOUT_GENERAL }
+			{ VK_NULL_HANDLE, ImageViews[0], VK_IMAGE_LAYOUT_GENERAL }
 		};
 		assert(!DescriptorSets.empty() && "");
 		assert(!DescriptorUpdateTemplates.empty() && "");
@@ -80,33 +80,21 @@ protected:
 		const auto Format = VK_FORMAT_R8G8B8A8_UINT;
 
 		{
-			_Images.resize(1);
+			Images.push_back(Image());
 			const auto Type = VK_IMAGE_TYPE_2D;
-
 			const VkExtent3D Extent3D = { 800, 600, 1 };
-
 			const auto Faces = 1;
 			const auto Layers = 1 * Faces;
 			const auto Levels = 1;
-
 			//!< ŽQl : VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT
-			CreateImage(&_Images[0], 0, Type, Format, Extent3D, Levels, Layers, VK_SAMPLE_COUNT_1_BIT, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_STORAGE_BIT);
-#if 0
-			AllocateImageMemory(&ImageDeviceMemory, Image, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-			VERIFY_SUCCEEDED(vkBindImageMemory(Device, Images[0], ImageDeviceMemory, 0));
-#else
-			uint32_t HeapIndex;
-			VkDeviceSize Offset;
-			SuballocateImageMemory(HeapIndex, Offset, _Images[0], VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-#endif
-		}
+			CreateImage(&Images.back().Image, 0, Type, Format, Extent3D, Levels, Layers, VK_SAMPLE_COUNT_1_BIT, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_STORAGE_BIT);
 
-		{
-			_ImageViews.resize(1);
-			const auto Type = VK_IMAGE_VIEW_TYPE_2D;
+			AllocateDeviceMemory(&Images.back().DeviceMemory, Images.back().Image, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+			VERIFY_SUCCEEDED(vkBindImageMemory(Device, Images.back().Image, Images.back().DeviceMemory, 0));
+
+			ImageViews.push_back(VkImageView());
 			const VkComponentMapping CompMap = { VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G, VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_A };
-
-			CreateImageView(&_ImageViews[0], _Images[0], Type, Format, CompMap, ImageSubresourceRange_ColorAll);
+			CreateImageView(&ImageViews.back(), Images.back().Image, VK_IMAGE_VIEW_TYPE_2D, Format, CompMap, ImageSubresourceRange_ColorAll);
 		}
 	}
 	
