8c86ce1b7d9c516d8fcc2b34d121e0d75126a695
horinoh <development@horino.sakura.ne.jp>
2021-05-11 00:06:49 +0900

    コード更新

--- MeshShaderDX/MeshShaderDX.ms.hlsl
+++ MeshShaderDX/MeshShaderDX.ms.hlsl
@@ -1,7 +1,11 @@
-struct OUT
+struct VERT_OUT
 {
     float4 Position : SV_POSITION;
     float3 Color : COLOR;
+};
+struct PRIM_OUT
+{
+    uint Id : INDEX0;
 };
 
 static const float3 Positions[] = { float3(0.0f, 0.5f, 0.0f), float3(-0.5f, -0.5f, 0.0f), float3(0.5f, -0.5f, 0.0f) };
@@ -9,21 +13,14 @@ static const float3 Colors[] = { float3(1.0f, 0.0f, 0.0f), float3(0.0f, 1.0f, 0.
 
 [numthreads(3, 1, 1)]
 [outputtopology("triangle")]
-//[outputtopology("line")]
-void main(uint ThreadID : SV_DispatchThreadID, uint GroupID : SV_GroupID, out vertices OUT Vertices[3], out indices uint3 Indices[1]/*, out primitives Prims[1]*/)
+void main(uint ThreadID : SV_GroupThreadID, uint GroupID : SV_GroupID, out indices uint3 Indices[1], out primitives PRIM_OUT Prims[1], out vertices VERT_OUT Vertices[3])
 {
     //!< スレッドグループから出力する、頂点数、プリミティブ数を宣言
     SetMeshOutputCounts(3, 1);
     
     Indices[0] = uint3(0, 1, 2);
-    //Prims[0] = 0;
+    Prims[0].Id = 0;
  
     Vertices[ThreadID].Position = float4(Positions[ThreadID], 1.0f);
     Vertices[ThreadID].Color = Colors[ThreadID];
-    //Vertices[0].Position = float4(Positions[0], 1.0f);
-    //Vertices[0].Color = Colors[0];
-    //Vertices[1].Position = float4(Positions[1], 1.0f);
-    //Vertices[1].Color = Colors[1];
-    //Vertices[2].Position = float4(Positions[2], 1.0f);
-    //Vertices[2].Color = Colors[2];       
 }
