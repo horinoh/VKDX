f0ebb24e64a998b020330c385429de144d391e77
horinoh <development@horino.sakura.ne.jp>
2021-05-20 02:33:07 +0900

    コメント追加

--- MeshletDX/MeshletDX.as.hlsl
+++ MeshletDX/MeshletDX.as.hlsl
@@ -5,17 +5,21 @@ struct PAYLOAD_OUT
 
 groupshared PAYLOAD_OUT Payload;
 
-[numthreads(32, 1, 1)]
+[numthreads(16, 1, 1)]
 void main(uint GroupThreadID : SV_GroupThreadID, uint GroupID : SV_GroupID)
 {
     bool Visible = true;
+    //bool Visible = 0 == (GroupThreadID % 2);
+    //bool Visible = 1 == (GroupThreadID % 2);
 
+    //!< Wave命令はスレッド間同期される
+    
     if (Visible)
     {
-        uint Index = WavePrefixCountBits(Visible);
-        Payload.MeshletIDs[Index] = GroupThreadID;
+        //!< WavePrefixCountBits() : true を返す(より若い)スレッド数を返す、結果的に true を返すスレッド ID が前詰めで格納されていく
+        Payload.MeshletIDs[WavePrefixCountBits(Visible)] = GroupThreadID;
     }
 
-    uint Count = WaveActiveCountBits(Visible);
-    DispatchMesh(Count, 1, 1, Payload);
+    //!< WaveActiveCountBits() : true を返す全スレッド数を返す、その分(メッシュレット数)だけメッシュシェーダを起動する(DispatchMesh())
+    DispatchMesh(WaveActiveCountBits(Visible), 1, 1, Payload);
 }
--- MeshletDX/MeshletDX.ms.hlsl
+++ MeshletDX/MeshletDX.ms.hlsl
@@ -8,25 +8,19 @@ struct VERT_OUT
     float4 Position : SV_POSITION;
     float3 Color : COLOR;
 };
-struct PRIM_OUT
-{
-    uint Id : INDEX0;
-};
 
 static const float3 Positions[] = { float3(0.0f, 0.5f, 0.0f), float3(-0.5f, -0.5f, 0.0f), float3(0.5f, -0.5f, 0.0f) };
 static const float3 Colors[] = { float3(1.0f, 0.0f, 0.0f), float3(0.0f, 1.0f, 0.0f), float3(0.0f, 0.0f, 1.0f), float3(1.0f, 1.0f, 0.0f), float3(0.0f, 1.0f, 1.0f), float3(1.0f, 0.0f, 1.0f), float3(1.0f, 1.0f, 1.0f), float3(0.0f, 0.0f, 0.0f) };
 
 [numthreads(3, 1, 1)]
 [outputtopology("triangle")]
-void main(uint GroupThreadID : SV_GroupThreadID, uint GroupID : SV_GroupID, in payload PAYLOAD_IN Payload, out indices uint3 Indices[1], out primitives PRIM_OUT Prims[1], out vertices VERT_OUT Vertices[3])
+void main(uint GroupThreadID : SV_GroupThreadID, uint GroupID : SV_GroupID, in payload PAYLOAD_IN Payload, out indices uint3 Indices[1], out vertices VERT_OUT Vertices[3])
 {
-    //!< スレッドグループから出力する、頂点数、プリミティブ数を宣言
     SetMeshOutputCounts(3, 1);
     
     Indices[0] = uint3(0, 1, 2);
-    Prims[0].Id = 0;
  
-    const float3 Offset = float3(GroupID, GroupID, 0.0f) / 32.0f - float3(0.5f, 0.5f, 0.0f);
+    const float3 Offset = float3(GroupID, GroupID, 0.0f) / 16.0f - float3(0.5f, 0.5f, 0.0f);
     Vertices[GroupThreadID].Position = float4(Positions[GroupThreadID] + Offset, 1.0f);
     Vertices[GroupThreadID].Color = Colors[Payload.MeshletIDs[GroupID] % 8];
 }
