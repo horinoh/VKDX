329cca2a27bb99826c182d1382c0d869731757d8
horinoh <development@horino.sakura.ne.jp>
2022-11-21 23:16:04 +0900

    コード更新

--- Colli.h
+++ Colli.h
@@ -415,28 +415,32 @@ namespace Colli
 	}
 
 	[[nodiscard]] static Vec2 SignedVolume(const Vec3& A, const Vec3& B) {
-		const auto AB = (B - A).Normalize();
+		const auto AB = B - A;
+		const auto U = AB.Normalize();
 		//!< 原点を、(選択した)軸に射影し、軸上での重心を求める
-		const auto P = A - AB * AB.Dot(A);
+		const auto P = A + U * U.Dot(-A);
 
 		//!< 3 軸の内、射影範囲が最大のものを見つける
-		auto MaxComp = 0;
-		for (auto i = 1; i < 3; ++i) {
-			if (std::abs(AB[MaxComp]) < std::abs(AB[i])) {
-				MaxComp = i;
+		auto MaxIndex = 0;
+		auto MaxSeg = 0.0f;
+		for (auto i = 0; i < 3; ++i) {
+			if (std::abs(MaxSeg) < std::abs(AB[i])) {
+				MaxSeg = AB[i];
+
+				MaxIndex = i;
 			}
 		}
 
-		const auto CompA = A[MaxComp];
-		const auto CompB = B[MaxComp];
-		const auto CompP = P[MaxComp];
+		//!< 対象の軸に射影
+		const std::array PrjSeg = { A[MaxIndex] , B[MaxIndex] };
+		const auto PrjP = P[MaxIndex];
 
 		//!< P が [A, B] (もしくは [B, A]) の間にある
-		if ((CompA < CompP && CompP < CompB) || (CompB < CompP && CompP < CompA)) {
-			return Vec2(CompB - CompP, CompP - CompA) / AB[MaxComp];
+		if ((PrjSeg[0] < PrjP && PrjP < PrjSeg[1]) || (PrjSeg[1] < PrjP && PrjP < PrjSeg[0])) {
+			return Vec2(PrjSeg[1] - PrjP, PrjP - PrjSeg[0]) / MaxSeg;
 		}
 		//!< P が A 側の外側
-		if ((CompA < CompB && CompP <= CompA) || (CompA >= CompB && CompP >= CompA)) {
+		if ((PrjSeg[0] < PrjSeg[1] && PrjP <= PrjSeg[0]) || (PrjSeg[0] >= PrjSeg[1] && PrjP >= PrjSeg[0])) {
 			return Vec2::AxisX();
 		}
 		//!< P が B 側の外側
@@ -449,39 +453,39 @@ namespace Colli
 		const auto P = N * A.Dot(N);
 
 		//!< 3 平面の内、射影面積が最大のものを見つける
-		auto MaxComp = 0;
+		auto MaxIndex = 0;
 		auto MaxArea = 0.0f;
 		for (auto i = 0; i < 3; ++i) {
-			const auto j = (i + 1) & 3;
-			const auto k = (i + 2) & 3;
+			const auto j = (i + 1) % 3;
+			const auto k = (i + 2) % 3;
 
 			const auto a = Vec2(A[j], A[k]);
 			const auto b = Vec2(B[j], B[k]);
 			const auto c = Vec2(C[j], C[k]);
 			const auto ab = b - a;
 			const auto ac = c - a;
-
 			const auto Area = ab.x() * ac.y() - ab.y() * ac.x();
 			if (std::abs(Area) > std::abs(MaxArea)) {
 				MaxArea = Area;
-				MaxComp = i;
+
+				MaxIndex = i;
 			}
 		}
 
 		//!< 対象の平面に射影
-		const auto x = (MaxComp + 1) % 3, y = (MaxComp + 2) % 3;
-		const std::array Planes = { Vec2(A[x], A[y]), Vec2(B[x], B[y]), Vec2(C[x], C[y]) };
-		const auto a = Vec2(P[x], P[y]);
+		const auto Index1 = (MaxIndex + 1) % 3;
+		const auto Index2 = (MaxIndex + 2) % 3;
+		const std::array PrjPlane = { Vec2(A[Index1], A[Index2]), Vec2(B[Index1], B[Index2]), Vec2(C[Index1], C[Index2]) };
+		const auto PrjP = Vec2(P[Index1], P[Index2]);
 
-		//!< 射影と辺からなるサブ三角形の面積
+		//!< 射影点と辺からなるサブ三角形の面積
 		Vec3 Areas;
 		for (auto i = 0; i < 3; ++i) {
 			const auto j = (i + 1) % 3;
 			const auto k = (i + 2) % 3;
 
-			const auto ab = Planes[j] - a;
-			const auto ac = Planes[k] - a;
-
+			const auto ab = PrjPlane[j] - PrjP;
+			const auto ac = PrjPlane[k] - PrjP;
 			Areas[i] = ab.x() * ac.y() - ab.y() * ac.x();
 		}
 
@@ -492,27 +496,26 @@ namespace Colli
 
 		//!< 3 辺に射影して一番近いものを見つける (1D の SignedVolume に帰着)
 		const std::array EdgesPts = { A, B, C };
-		auto Lambda = Vec3::AxisX();
-		auto MaxLenSq = (std::numeric_limits<float>::max)();
+		Vec3 Lambda;
+		auto MinLenSq = (std::numeric_limits<float>::max)();
 		for (auto i = 0; i < 3; ++i) {
 			const auto j = (i + 1) % 3;
 			const auto k = (i + 2) % 3;
 
 			const auto LambdaEdge = SignedVolume(EdgesPts[j], EdgesPts[k]);
 			const auto LenSq = (EdgesPts[j] * LambdaEdge[0] + EdgesPts[k] * LambdaEdge[1]).LengthSq();
-			if (LenSq < MaxLenSq) {
-				Lambda = Vec3(0.0f, LambdaEdge[0], LambdaEdge[1]);
-				MaxLenSq = LenSq;
+			if (LenSq < MinLenSq) {
+				Lambda[i] = 0.0f;
+				Lambda[j] = LambdaEdge[0];
+				Lambda[k] = LambdaEdge[1];
+
+				MinLenSq = LenSq;
 			}
 		}
 		return Lambda;
 	}
 	[[nodiscard]] static Vec4 SignedVolume(const Vec3& A, const Vec3& B, const Vec3& C, const Vec3& D) {
-		const auto M = Mat4({ A.x(), B.x(), C.x(), D.x() }, { A.y(), B.y(), C.y(), D.y() }, { A.z(), B.z(), C.z(), D.z() }, Vec4::One());
-		const auto Cofactor = Vec4(-Mat3({ M[1][0], M[1][1], M[1][2] }, { M[2][0], M[2][1], M[2][2] }, { M[3][0], M[3][1], M[3][2] }).Determinant(),
-			Mat3({ M[0][0], M[0][1], M[0][2] }, { M[2][0], M[2][1], M[2][2] }, { M[3][0], M[3][1], M[3][2] }).Determinant(),
-			-Mat3({ M[0][0], M[0][1], M[0][2] }, { M[1][0], M[1][1], M[1][2] }, { M[3][0], M[3][1], M[3][2] }).Determinant(),
-			Mat3({ M[0][0], M[0][1], M[0][2] }, { M[1][0], M[1][1], M[1][2] }, { M[2][0], M[2][1], M[2][2] }).Determinant());
+		const auto Cofactor = Vec4(-Mat3(B, C, D).Determinant(), Mat3(A, C, D).Determinant(), -Mat3(A, B, D).Determinant(), Mat3(A, B, C).Determinant());
 		const auto Det = Cofactor.x() + Cofactor.y() + Cofactor.z() + Cofactor.w();
 
 		//!< 四面体内部にあれば、重心座標を返す
@@ -522,17 +525,22 @@ namespace Colli
 
 		//!< 3 面に射影して一番近いものを見つける (2D の SignedVolume に帰着)
 		const std::array FacePts = { A, B, C, D };
-		auto Lambda = Vec4::AxisX();
-		auto MaxLenSq = (std::numeric_limits<float>::max)();
+		Vec4 Lambda;
+		auto MinLenSq = (std::numeric_limits<float>::max)();
 		for (auto i = 0; i < 4; ++i) {
 			const auto j = (i + 1) % 4;
 			const auto k = (i + 2) % 4;
+			const auto l = (i + 3) % 4;
 
 			const auto LambdaFace = SignedVolume(FacePts[i], FacePts[j], FacePts[k]);
 			const auto LenSq = (FacePts[i] * LambdaFace[0] + FacePts[j] * LambdaFace[1] + FacePts[k] * LambdaFace[2]).LengthSq();
-			if (LenSq < MaxLenSq) {
-				Lambda = Vec4(0.0f, LambdaFace[0], LambdaFace[1], LambdaFace[2]);
-				MaxLenSq = LenSq;
+			if (LenSq < MinLenSq) {
+				Lambda[i] = LambdaFace[0];
+				Lambda[j] = LambdaFace[1];
+				Lambda[k] = LambdaFace[2];
+				Lambda[l] = 0.0f;
+
+				MinLenSq = LenSq;
 			}
 		}
 		return Lambda;
